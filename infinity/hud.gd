109e62115ffa97f7aa39de291a59cf3932e7cc54
Garrett Turner <118405391+gturner9371213@users.noreply.github.com>
2024-11-05 18:37:37 -0700

    Added back to start button
    Added back to start buttorn

--- infinity/hud.gd
+++ infinity/hud.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 signal start_game
+# No need to use back_to_start signal
 
 func show_message(text):
 	$Message.text = text
@@ -10,21 +11,20 @@ func show_message(text):
 func show_game_over():
 	print("show_game_over() called in hud.gd")
 	show_message("Game Over")
-	await $MessageTimer.timeout  # Wait for the timer to finish
-	await get_tree().create_timer(2.0).timeout  # Wait for 2 seconds before showing the start button
-	$StartButton.show()  # Show the start button after the delay
+	await $MessageTimer.timeout
+	await get_tree().create_timer(2.0).timeout
+	$StartButton.show()
+	$BackToStartButton.hide()
 
+# Function called when the "Start" button is pressed
 func _on_start_button_pressed():
 	$StartButton.hide()
+	$BackToStartButton.show()
 	start_game.emit()
+
 
 func _on_message_timer_timeout():
 	$Message.hide()
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass  # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	$BackToStartButton.hide()
--- infinity/main.gd
+++ infinity/main.gd
@@ -6,12 +6,21 @@ var spawn_wait_time = 2
 var speed_range_min = 150
 var speed_range_max = 250
 
-#func game_over():
-#	print("game_over() function called in main.gd")
-#	$HUD.show_game_over()
-#	$MobTimer.stop()
+# Function to return to the start menu
+func return_to_start_menu():
+	print("Returning to start menu...")
+	$Player.hide()
+	get_tree().call_group("mobs", "queue_free")
+	$HUD.show_message("Press Start to Begin")
+	
+	# Attempt to show StartButton and hide BackToStartButton
+	if $HUD.has_node("StartButton") and $HUD.has_node("BackToStartButton"):
+		$HUD.get_node("StartButton").show()
+		$HUD.get_node("BackToStartButton").hide()
+	else:
+		print("Error: StartButton or BackToStartButton not found.")
 
-#Begins the game
+# Begins the game
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
@@ -19,42 +28,29 @@ func new_game():
 	get_tree().call_group("mobs", "queue_free")
 	$HUD.show_message("Get Ready")
 
-#Begin the mob timer
 func _on_start_timer_timeout():
 	$MobTimer.start()
 
-#Spawn the mobs
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
-	
 	var mob_spawn_location = $MobPath/MobSpawnLocation
 	mob_spawn_location.progress_ratio = randf()
-
 	var direction = mob_spawn_location.rotation + PI / 2
-	
 	mob.position = mob_spawn_location.position
-
 	direction += randf_range(-PI / 4, PI / 4)
 	mob.rotation = direction
-
 	var velocity = Vector2(randf_range(speed_range_min, speed_range_max), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
-
 	add_child(mob)
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time
 func _ready() -> void:
+	# Optional: Connect button signals here if not done in the editor
 	pass
-	#new_game() # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
-
-func _on_spawn_increase_timer_timeout() -> void:
-	if speed_range_min < 500:
-		speed_range_min += 20
-	if speed_range_max < 700:
-		speed_range_max += 20
+# Function for Back to Start button
+func _on_back_to_start_button_pressed() -> void:
+	print("Back to Start button clicked")  # Debugging statement
+	$HUD.get_node("StartButton").show()  # Show the start button
+	$HUD.get_node("BackToStartButton").hide()  # Hide the back to start button
+	return_to_start_menu()
